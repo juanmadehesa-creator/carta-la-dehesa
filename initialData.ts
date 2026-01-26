8e0e888b0f69f87a3762f49a1e09968fea8b20c7
Manolo Recio <manoloreciodesign@gmail.com>
2026-01-26 10:11:14 +0100

    updates

---
+++ initialData.ts
@@ -0,0 +1,695 @@
+import { Dish } from '@/types';
+
+export const initialDishes: Dish[] = [
+  {
+    "id": "1",
+    "name": "Mojama de Barbate / Cured Tuna Loin",
+    "price": 4,
+    "pricePlatter": 7.9,
+    "description": "De Barbate",
+    "category": "DE PICOTEO / BITES",
+    "isAvailable": true,
+    "isVisible": true
+  },
+  {
+    "id": "2",
+    "name": "Chicharrones de la casa / Crispy Pork Belly",
+    "price": 3.2,
+    "pricePlatter": 0,
+    "description": "Estilo sevillano",
+    "category": "DE PICOTEO / BITES",
+    "isAvailable": true,
+    "isVisible": true
+  },
+  {
+    "id": "3",
+    "name": "Almendras Fritas / Fried Almonds",
+    "price": 2.5,
+    "pricePlatter": 0,
+    "description": "Saladitas",
+    "category": "DE PICOTEO / BITES",
+    "isAvailable": true,
+    "isVisible": true
+  },
+  {
+    "id": "4",
+    "name": "Huevas de Maruca / Cured Hake Roe",
+    "price": 4,
+    "pricePlatter": 7.9,
+    "description": "Calidad extra",
+    "category": "DE PICOTEO / BITES",
+    "isAvailable": true,
+    "isVisible": true
+  },
+  {
+    "id": "5",
+    "name": "Queso Oveja (ZAMORANO) / Sheep Cheese",
+    "price": 3.8,
+    "pricePlatter": 8.95,
+    "description": "D.O.P. Zamora",
+    "category": "DE PICOTEO / BITES",
+    "isAvailable": true,
+    "isVisible": true
+  },
+  {
+    "id": "6",
+    "name": "Salchichón de Bellota a tacos / Diced Sausage",
+    "price": 3.2,
+    "pricePlatter": 6.5,
+    "description": "Ibérico de bellota",
+    "category": "DE PICOTEO / BITES",
+    "isAvailable": true,
+    "isVisible": true
+  },
+  {
+    "id": "7",
+    "name": "Caña de Lomo de Bellota (BEHER) / Cured Pork Loin",
+    "price": 3.8,
+    "pricePlatter": 8.95,
+    "description": "Marca BEHER",
+    "category": "DE PICOTEO / BITES",
+    "isAvailable": true,
+    "isVisible": true
+  },
+  {
+    "id": "8",
+    "name": "Queso Especial Cabra (ZAMORANO) / Goat Cheese",
+    "price": 3.8,
+    "pricePlatter": 8.95,
+    "description": "D.O.P. Zamora",
+    "category": "DE PICOTEO / BITES",
+    "isAvailable": true,
+    "isVisible": true
+  },
+  {
+    "id": "9",
+    "name": "Jamón Ibérico de Bellota / Acorn-Fed Ham",
+    "price": 0,
+    "pricePlatter": 15.95,
+    "description": "Corte artesano",
+    "category": "DE PICOTEO / BITES",
+    "isAvailable": true,
+    "isVisible": true
+  },
+  {
+    "id": "10",
+    "name": "Jamón Ibérico de Recebo / Iberian Ham",
+    "price": 3.95,
+    "pricePlatter": 8.95,
+    "description": "Calidad superior",
+    "category": "DE PICOTEO / BITES",
+    "isAvailable": true,
+    "isVisible": true
+  },
+  {
+    "id": "11",
+    "name": "Bravas (3 salsas) / Spicy Potatoes",
+    "price": 3.2,
+    "pricePlatter": 5.95,
+    "description": "Con 3 salsas",
+    "category": "PARA COMPARTIR / TO SHARE",
+    "isAvailable": true,
+    "isVisible": true
+  },
+  {
+    "id": "12",
+    "name": "Revuelto Bacalao Dorado LA DEHESA / Scrambled Cod",
+    "price": 0,
+    "pricePlatter": 11.95,
+    "description": "Especialidad de la casa",
+    "category": "PARA COMPARTIR / TO SHARE",
+    "isAvailable": true,
+    "isVisible": true
+  },
+  {
+    "id": "13",
+    "name": "Croquetas Caseras / Homemade Croquettes",
+    "price": 3.8,
+    "pricePlatter": 9.95,
+    "description": "Jamón, Mejillones o Cola de Toro",
+    "category": "PARA COMPARTIR / TO SHARE",
+    "isAvailable": true,
+    "isVisible": true
+  },
+  {
+    "id": "14",
+    "name": "Calamares de Campo con Langostinos / Fried Medley",
+    "price": 0,
+    "pricePlatter": 10.8,
+    "description": "Con langostinos",
+    "category": "PARA COMPARTIR / TO SHARE",
+    "isAvailable": true,
+    "isVisible": true
+  },
+  {
+    "id": "15",
+    "name": "Revuelto de Espárragos Trigueros / Scrambled Asparagus",
+    "price": 0,
+    "pricePlatter": 11.95,
+    "description": "Con Ibérico",
+    "category": "PARA COMPARTIR / TO SHARE",
+    "isAvailable": true,
+    "isVisible": true
+  },
+  {
+    "id": "16",
+    "name": "Revuelto de Habitas Baby / Baby Broad Beans",
+    "price": 0,
+    "pricePlatter": 12.95,
+    "description": "Con Jamón",
+    "category": "PARA COMPARTIR / TO SHARE",
+    "isAvailable": true,
+    "isVisible": true
+  },
+  {
+    "id": "17",
+    "name": "Revuelto de Setas / Mushroom Scramble",
+    "price": 0,
+    "pricePlatter": 12.95,
+    "description": "Con Oloroso y Jamón",
+    "category": "PARA COMPARTIR / TO SHARE",
+    "isAvailable": true,
+    "isVisible": true
+  },
+  {
+    "id": "18",
+    "name": "Noruego / Norwegian",
+    "price": 3.25,
+    "pricePlatter": 0,
+    "description": "Salmón, Queso viejo y Alioli",
+    "category": "MONTAÍTOS / ROLLS",
+    "isAvailable": true,
+    "isVisible": true
+  },
+  {
+    "id": "19",
+    "name": "Chorizo Picante / Spicy Chorizo",
+    "price": 3.25,
+    "pricePlatter": 0,
+    "description": "Sabor intenso",
+    "category": "MONTAÍTOS / ROLLS",
+    "isAvailable": true,
+    "isVisible": true
+  },
+  {
+    "id": "20",
+    "name": "El Feriante / The Fairgoer",
+    "price": 3.25,
+    "pricePlatter": 0,
+    "description": "Lomo con Jamón ibérico",
+    "category": "MONTAÍTOS / ROLLS",
+    "isAvailable": true,
+    "isVisible": true
+  },
+  {
+    "id": "21",
+    "name": "Mini Serranito / Small Serranito",
+    "price": 3.6,
+    "pricePlatter": 0,
+    "description": "Pimiento frito, Lomo y Jamón",
+    "category": "MONTAÍTOS / ROLLS",
+    "isAvailable": true,
+    "isVisible": true
+  },
+  {
+    "id": "22",
+    "name": "Melva y Pimiento / Melva Tuna & Pepper",
+    "price": 3.25,
+    "pricePlatter": 0,
+    "description": "Con pimiento morrón",
+    "category": "MONTAÍTOS / ROLLS",
+    "isAvailable": true,
+    "isVisible": true
+  },
+  {
+    "id": "23",
+    "name": "Islandés / Icelandic",
+    "price": 3.25,
+    "pricePlatter": 0,
+    "description": "Bacalao Ahumado y Salmorejo",
+    "category": "MONTAÍTOS / ROLLS",
+    "isAvailable": true,
+    "isVisible": true
+  },
+  {
+    "id": "24",
+    "name": "El Alfarero / The Potter",
+    "price": 3.25,
+    "pricePlatter": 0,
+    "description": "Lomo orza, Queso viejo y Mojo picón",
+    "category": "MONTAÍTOS / ROLLS",
+    "isAvailable": true,
+    "isVisible": true
+  },
+  {
+    "id": "25",
+    "name": "Salmorejo Cordobés / Cold Tomato Soup",
+    "price": 3.7,
+    "pricePlatter": 7.95,
+    "description": "Receta tradicional",
+    "category": "FRESQUITOS / CHILLED PLATES",
+    "isAvailable": true,
+    "isVisible": true
+  },
+  {
+    "id": "26",
+    "name": "Patatas con Ali-oli casero / Garlic Mayo Potatoes",
+    "price": 3.7,
+    "pricePlatter": 7.95,
+    "description": "Ali-oli artesano",
+    "category": "FRESQUITOS / CHILLED PLATES",
+    "isAvailable": true,
+    "isVisible": true
+  },
+  {
+    "id": "27",
+    "name": "Tomate Rosa con Melva / Pink Tomato & Tuna",
+    "price": 0,
+    "pricePlatter": 7.95,
+    "description": "Melva Canutera",
+    "category": "FRESQUITOS / CHILLED PLATES",
+    "isAvailable": true,
+    "isVisible": true
+  },
+  {
+    "id": "28",
+    "name": "Salpicón de Marisco / Seafood Salad",
+    "price": 3.95,
+    "pricePlatter": 9.95,
+    "description": "Vinagreta clásica",
+    "category": "FRESQUITOS / CHILLED PLATES",
+    "isAvailable": true,
+    "isVisible": true
+  },
+  {
+    "id": "29",
+    "name": "Ensaladilla de Gambas / Shrimp Salad",
+    "price": 3.95,
+    "pricePlatter": 9.95,
+    "description": "Gambas frescas",
+    "category": "FRESQUITOS / CHILLED PLATES",
+    "isAvailable": true,
+    "isVisible": true
+  },
+  {
+    "id": "30",
+    "name": "Papas aliñás con Melva / Seasoned Potatoes",
+    "price": 3.95,
+    "pricePlatter": 9.95,
+    "description": "Con Melva canutera",
+    "category": "FRESQUITOS / CHILLED PLATES",
+    "isAvailable": true,
+    "isVisible": true
+  },
+  {
+    "id": "31",
+    "name": "Hojaldre de Queso y Salmón / Salmon Puff Pastry",
+    "price": 5.95,
+    "pricePlatter": 0,
+    "description": "Queso de cabra y salmón",
+    "category": "FRESQUITOS / CHILLED PLATES",
+    "isAvailable": true,
+    "isVisible": true
+  },
+  {
+    "id": "32",
+    "name": "Chocos Fritos / Fried Cuttlefish",
+    "price": 3.95,
+    "pricePlatter": 10.8,
+    "description": "Harina de calidad",
+    "category": "DE LA MAR / SEAFOOD",
+    "isAvailable": true,
+    "isVisible": true
+  },
+  {
+    "id": "33",
+    "name": "Pavías de Bacalao / Cod Fritters",
+    "price": 2,
+    "pricePlatter": 0,
+    "description": "Precio por unidad",
+    "category": "DE LA MAR / SEAFOOD",
+    "isAvailable": true,
+    "isVisible": true
+  },
+  {
+    "id": "34",
+    "name": "Adobo Andaluz / Marinated Fried Fish",
+    "price": 3.95,
+    "pricePlatter": 10.8,
+    "description": "Receta sevillana",
+    "category": "DE LA MAR / SEAFOOD",
+    "isAvailable": true,
+    "isVisible": true
+  },
+  {
+    "id": "35",
+    "name": "Puntillitas / Fried Baby Squid",
+    "price": 3.95,
+    "pricePlatter": 10.8,
+    "description": "Crujientes",
+    "category": "DE LA MAR / SEAFOOD",
+    "isAvailable": true,
+    "isVisible": true
+  },
+  {
+    "id": "36",
+    "name": "Gambas al Ajillo / Garlic Prawns",
+    "price": 0,
+    "pricePlatter": 12.95,
+    "description": "Con guindilla",
+    "category": "DE LA MAR / SEAFOOD",
+    "isAvailable": true,
+    "isVisible": true
+  },
+  {
+    "id": "37",
+    "name": "Pulpo a la Gallega / Galician Octopus",
+    "price": 0,
+    "pricePlatter": 18.95,
+    "description": "Con pimentón y cachelos",
+    "category": "DE LA MAR / SEAFOOD",
+    "isAvailable": true,
+    "isVisible": true
+  },
+  {
+    "id": "38",
+    "name": "Chanquetada con Huevo / Fried Whitebait",
+    "price": 0,
+    "pricePlatter": 12.95,
+    "description": "Con huevo y pimiento",
+    "category": "DE LA MAR / SEAFOOD",
+    "isAvailable": true,
+    "isVisible": true
+  },
+  {
+    "id": "39",
+    "name": "Mero frito al limón / Fried Grouper",
+    "price": 3.95,
+    "pricePlatter": 10.8,
+    "description": "Con toque de limón",
+    "category": "DE LA MAR / SEAFOOD",
+    "isAvailable": true,
+    "isVisible": true
+  },
+  {
+    "id": "40",
+    "name": "Taquitos de Bacalao / Fried Cod Bites",
+    "price": 4.5,
+    "pricePlatter": 12.95,
+    "description": "Sin espinas",
+    "category": "DE LA MAR / SEAFOOD",
+    "isAvailable": true,
+    "isVisible": true
+  },
+  {
+    "id": "41",
+    "name": "Jamón Ibérico con Salmorejo / Ham Toast",
+    "price": 10.95,
+    "pricePlatter": 0,
+    "description": "Sobre pan de cristal",
+    "category": "TABLA DE PANES / ARTISAN TOASTS",
+    "isAvailable": true,
+    "isVisible": true
+  },
+  {
+    "id": "42",
+    "name": "De la Casa / Our Special Toast",
+    "price": 14.95,
+    "pricePlatter": 0,
+    "description": "Solomillo, Jamón y Huevo codorniz",
+    "category": "TABLA DE PANES / ARTISAN TOASTS",
+    "isAvailable": true,
+    "isVisible": true
+  },
+  {
+    "id": "43",
+    "name": "Bacalao ahumado / Smoked Cod Toast",
+    "price": 10.95,
+    "pricePlatter": 0,
+    "description": "Calidad superior",
+    "category": "TABLA DE PANES / ARTISAN TOASTS",
+    "isAvailable": true,
+    "isVisible": true
+  },
+  {
+    "id": "44",
+    "name": "Lomo de Orza y Queso viejo / Pork Loin Toast",
+    "price": 10.95,
+    "pricePlatter": 0,
+    "description": "Lomo artesano",
+    "category": "TABLA DE PANES / ARTISAN TOASTS",
+    "isAvailable": true,
+    "isVisible": true
+  },
+  {
+    "id": "45",
+    "name": "Melva y Anchoas / Tuna & Anchovies Toast",
+    "price": 14.95,
+    "pricePlatter": 0,
+    "description": "Anchoas del Cantábrico",
+    "category": "TABLA DE PANES / ARTISAN TOASTS",
+    "isAvailable": true,
+    "isVisible": true
+  },
+  {
+    "id": "46",
+    "name": "Solomillo al Whisky / Whiskey Pork",
+    "price": 4.5,
+    "pricePlatter": 14.95,
+    "description": "Salsa clásica",
+    "category": "CARNÍVORO / MEAT LOVER'S",
+    "isAvailable": true,
+    "isVisible": true
+  },
+  {
+    "id": "47",
+    "name": "Carrillada al Chocolate / Pork Cheek",
+    "price": 4.5,
+    "pricePlatter": 12.95,
+    "description": "Ibérico en salsa chocolate",
+    "category": "CARNÍVORO / MEAT LOVER'S",
+    "isAvailable": true,
+    "isVisible": true
+  },
+  {
+    "id": "48",
+    "name": "Presa Ibérica de Bellota / Iberian Pork",
+    "price": 4.95,
+    "pricePlatter": 14.95,
+    "description": "Con Jamón",
+    "category": "CARNÍVORO / MEAT LOVER'S",
+    "isAvailable": true,
+    "isVisible": true
+  },
+  {
+    "id": "49",
+    "name": "Lagartito Ibérico / Pork Skirt Steak",
+    "price": 4.8,
+    "pricePlatter": 10.95,
+    "description": "Con Jamón",
+    "category": "CARNÍVORO / MEAT LOVER'S",
+    "isAvailable": true,
+    "isVisible": true
+  },
+  {
+    "id": "50",
+    "name": "BIG Hamburguesa LA DEHESA / Big Burger",
+    "price": 0,
+    "pricePlatter": 9.95,
+    "description": "Receta especial",
+    "category": "CARNÍVORO / MEAT LOVER'S",
+    "isAvailable": true,
+    "isVisible": true
+  },
+  {
+    "id": "51",
+    "name": "Pluma Ibérica con Jamón / Pork Feather Loin",
+    "price": 0,
+    "pricePlatter": 12.95,
+    "description": "Jugosa y sabrosa",
+    "category": "CARNÍVORO / MEAT LOVER'S",
+    "isAvailable": true,
+    "isVisible": true
+  },
+  {
+    "id": "52",
+    "name": "Bastoncitos de Secreto / Pork Strips",
+    "price": 4.8,
+    "pricePlatter": 11.95,
+    "description": "Con Jamón",
+    "category": "CARNÍVORO / MEAT LOVER'S",
+    "isAvailable": true,
+    "isVisible": true
+  },
+  {
+    "id": "53",
+    "name": "San Jacobo Ibérico / Iberian Cordon Bleu",
+    "price": 5.95,
+    "pricePlatter": 0,
+    "description": "Casero",
+    "category": "CARNÍVORO / MEAT LOVER'S",
+    "isAvailable": true,
+    "isVisible": true
+  },
+  {
+    "id": "54",
+    "name": "Chuletitas de Cordero / Lamb Chops",
+    "price": 5.95,
+    "pricePlatter": 17.95,
+    "description": "Cordero Lechal",
+    "category": "CARNÍVORO / MEAT LOVER'S",
+    "isAvailable": true,
+    "isVisible": true
+  },
+  {
+    "id": "55",
+    "name": "Manitas de Cerdo / Pork Trotters",
+    "price": 4.5,
+    "pricePlatter": 0,
+    "description": "Receta de la Abuela (unidad)",
+    "category": "DE ZAMORA Y ASTURIAS / TASTE OF ZAMORA & ASTURIAS",
+    "isAvailable": true,
+    "isVisible": true
+  },
+  {
+    "id": "56",
+    "name": "Callos de Ternera / Beef Tripe",
+    "price": 3.95,
+    "pricePlatter": 9.95,
+    "description": "Los mejores de la casa",
+    "category": "DE ZAMORA Y ASTURIAS / TASTE OF ZAMORA & ASTURIAS",
+    "isAvailable": true,
+    "isVisible": true
+  },
+  {
+    "id": "57",
+    "name": "Matanza de Zamora / Zamora Pork Feast",
+    "price": 3.95,
+    "pricePlatter": 9.95,
+    "description": "Con Huevo frito",
+    "category": "DE ZAMORA Y ASTURIAS / TASTE OF ZAMORA & ASTURIAS",
+    "isAvailable": true,
+    "isVisible": true
+  },
+  {
+    "id": "58",
+    "name": "Mini Cachopo Asturiano / Mini Cachopo",
+    "price": 0,
+    "pricePlatter": 9.95,
+    "description": "Clásico",
+    "category": "DE ZAMORA Y ASTURIAS / TASTE OF ZAMORA & ASTURIAS",
+    "isAvailable": true,
+    "isVisible": true
+  },
+  {
+    "id": "59",
+    "name": "Cachopo Asturiano (400gr) / Large Cachopo",
+    "price": 0,
+    "pricePlatter": 18.95,
+    "description": "Aproximadamente 400gr",
+    "category": "DE ZAMORA Y ASTURIAS / TASTE OF ZAMORA & ASTURIAS",
+    "isAvailable": true,
+    "isVisible": true
+  },
+  {
+    "id": "60",
+    "name": "Habitas Baby con Jamón / Baby Broad Beans",
+    "price": 3.95,
+    "pricePlatter": 9.95,
+    "description": "Con taquitos de Ibérico",
+    "category": "DE ZAMORA Y ASTURIAS / TASTE OF ZAMORA & ASTURIAS",
+    "isAvailable": true,
+    "isVisible": true
+  },
+  {
+    "id": "61",
+    "name": "Cerveza Caña / Draught Beer",
+    "price": 1.5,
+    "pricePlatter": 0,
+    "description": "Copa pequeña",
+    "category": "BEBIDAS / BEVERAGES",
+    "isAvailable": true,
+    "isVisible": true
+  },
+  {
+    "id": "62",
+    "name": "Copa Rioja Crianza / Rioja Wine",
+    "price": 3.2,
+    "pricePlatter": 0,
+    "description": "Vino de la casa",
+    "category": "BEBIDAS / BEVERAGES",
+    "isAvailable": true,
+    "isVisible": true
+  },
+  {
+    "id": "63",
+    "name": "Copa Ribera del Duero / Ribera Wine",
+    "price": 3.2,
+    "pricePlatter": 0,
+    "description": "Vino de la casa",
+    "category": "BEBIDAS / BEVERAGES",
+    "isAvailable": true,
+    "isVisible": true
+  },
+  {
+    "id": "64",
+    "name": "Copa Manzanilla en Rama / Sherry",
+    "price": 1.6,
+    "pricePlatter": 0,
+    "description": "Especialidad",
+    "category": "BEBIDAS / BEVERAGES",
+    "isAvailable": true,
+    "isVisible": true
+  },
+  {
+    "id": "65",
+    "name": "Combinados de Ron / Rum Mixers",
+    "price": 6,
+    "pricePlatter": 0,
+    "description": "Desde / From 6,00 €",
+    "category": "LICORES / SPIRITS & SHOTS",
+    "isAvailable": true,
+    "isVisible": true
+  },
+  {
+    "id": "66",
+    "name": "Combinados de Whisky / Whisky Mixers",
+    "price": 6,
+    "pricePlatter": 0,
+    "description": "Desde / From 6,00 €",
+    "category": "LICORES / SPIRITS & SHOTS",
+    "isAvailable": true,
+    "isVisible": true
+  },
+  {
+    "id": "67",
+    "name": "Combinados de Ginebra / Gin Mixers",
+    "price": 6,
+    "pricePlatter": 0,
+    "description": "Desde / From 6,00 €",
+    "category": "LICORES / SPIRITS & SHOTS",
+    "isAvailable": true,
+    "isVisible": true
+  },
+  {
+    "id": "68",
+    "name": "Chupitos / Shots",
+    "price": 2.5,
+    "pricePlatter": 0,
+    "description": "Desde / From 2,50 €",
+    "category": "LICORES / SPIRITS & SHOTS",
+    "isAvailable": true,
+    "isVisible": true
+  },
+  {
+    "id": "1769417159310",
+    "name": "Cola de Toro / Spanish oxtail stew",
+    "price": 0,
+    "pricePlatter": 6.95,
+    "description": "",
+    "category": "FUERA DE CARTA / DAILY SPECIALS",
+    "isAvailable": true,
+    "isVisible": true,
+    "allergens": []
+  }
+];
